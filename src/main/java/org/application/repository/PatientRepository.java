f4d5de3d2394201a607e8f1db5b9868cfc6823b2
PrincySingh09 <139205001+PrincySingh09@users.noreply.github.com>
2024-01-19 13:21:27 +0530

    PatientRepository.java

--- src/main/java/org/application/repository/PatientRepository.java
+++ src/main/java/org/application/repository/PatientRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-    // rewrite findbyId to return an exception if the patient is not found
     default Patient findByIdOrThrow(Long id) {
         return findById(id).orElseThrow(() -> new UserNotFoundException("Patient not found"));
     }
